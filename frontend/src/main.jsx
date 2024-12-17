e45d71a26f7ab182529cc0bcf01f971c619ef540
Yousefchaabi <yousef.chaabii@gmail.com>
2024-12-17 14:54:04 +0100

    category actions created successfully

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -19,6 +19,7 @@ import Register from "./pages/Auth/Register.jsx";
 import Profile from "./pages/User/Profile.jsx";
 import AdminRoute from "./pages/Admin/AdminRoute.jsx";
 import UserList from "./pages/Admin/UserList.jsx";
+import CategoryList from "./pages/Admin/CategoryList.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -34,6 +35,7 @@ const router = createBrowserRouter(
       {/* Registered users */}
       <Route path="/admin" element={<AdminRoute />}>
         <Route path="userlist" element={<UserList />} />
+        <Route path="categorylist" element={<CategoryList />} />
       </Route>
     </Route>
   )
